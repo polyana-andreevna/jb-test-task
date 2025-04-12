32edc42e4efd2be9ea063c897b4c4414385e7c19
Polina Nebova <polina.nebova@gmail.com>
2025-04-12 18:47:16 +0200

    MRR and ARR charts were created

--- metrics/mrr_and_arr.sql
+++ metrics/mrr_and_arr.sql
@@ -1,48 +1,52 @@
-with t1 as (select *,
+WITH t1 AS (SELECT *,
                    ROUND((amount_in_usd * (- 1 / days_in_month * payment_day + (days_in_month + 1) / days_in_month) /
-                          subscription_duration)::numeric, 2)                 as first_payment,
-                   ROUND((amount_in_usd / subscription_duration)::numeric, 2) as rest_payment
-            from (select customer,
+                          subscription_duration)::numeric, 2)                 AS first_payment,
+                   ROUND((amount_in_usd / subscription_duration)::numeric, 2) AS rest_payment
+            FROM (SELECT customer,
                          quantity,
                          processed_date,
                          amount_in_usd,
-                         to_char(processed_date, 'DD')::numeric as payment_day,
-                         to_char(processed_date, 'MM')::numeric as month_payment,
-                         date_part('days',
-                                   date_trunc('month', processed_date)
-                                       + interval '1 month'
-                                       - interval '1 day')      as days_in_month,
-                         case
-                             when (discount_in_usd + amount_in_usd)::numeric / quantity::numeric < 35 then 1
-                             else 12
-                             end                                as subscription_duration,
-                         row_number() over ()                   as payment_id
-                  from product_sales
-                  where customer in (select customer
-                                     from product_sales
-                                     where customer_status = 'new customer'
-                                     group by customer
-                                     having count(*) = 1)) subscription_type)
-select to_char(to_char(processed_date, 'YYYY-MM-01')::date + (index || ' months')::INTERVAL, 'YYYY-MM') as dt, -- MRR
---        to_char((processed_date + (index || ' months')::INTERVAL)::date, 'YYYY')                         as dt, -- ARR
-       sum(value)                                                                                       as sum
-from (select *,
-             payment_id,
-             payment_day,
-             amount_in_usd,
-             gs.generate_index as index,
-             first_payment,
-             CASE
-                 WHEN gs.generate_index = 0 THEN first_payment
-                 WHEN gs.generate_index = subscription_duration THEN
-                     ROUND((amount_in_usd - (first_payment + (subscription_duration - 1) * rest_payment))::numeric, 2)
-                 ELSE rest_payment
-                 END           AS value
-      from t1,
-           LATERAL (
-               SELECT generate_series(0, subscription_duration) AS generate_index
-               ) gs) t2
-group by dt
-order by dt;
+                         TO_CHAR(processed_date, 'DD')::numeric AS payment_day,
+                         TO_CHAR(processed_date, 'MM')::numeric AS month_payment,
+                         DATE_PART('days',
+                                   DATE_TRUNC('month', processed_date)
+                                       + INTERVAL '1 month'
+                                       - INTERVAL '1 day')      AS days_in_month,
+                         CASE
+                             WHEN (discount_in_usd + amount_in_usd)::numeric / quantity::numeric < 35 THEN 1
+                             ELSE 12
+                             END                                AS subscription_duration,
+                         ROW_NUMBER() OVER ()                   AS payment_id
+                  FROM product_sales
+                  WHERE customer IN (SELECT customer
+                                     FROM product_sales
+                                     WHERE customer_status = 'new customer'
+                                     GROUP BY customer
+                                     HAVING COUNT(*) = 1)) subscription_type),
+     t2 AS (SELECT t1.*,
+                   gs.index AS index,
+                   CASE
+                       WHEN gs.index = 0 THEN first_payment
+                       WHEN gs.index = subscription_duration THEN
+                           ROUND((amount_in_usd -
+                                  (first_payment + (subscription_duration - 1) * rest_payment))::numeric, 2)
+                       ELSE rest_payment
+                       END  AS value
+            FROM t1
+                     CROSS JOIN LATERAL (
+                SELECT GENERATE_SERIES(0, subscription_duration) AS index
+                ) gs)
+SELECT 'MRR'                                                                                            AS metric_name,
+       TO_CHAR(TO_CHAR(processed_date, 'YYYY-MM-01')::date + (index || ' months')::INTERVAL, 'YYYY-MM') AS dt,
+       SUM(value)
+FROM t2
+GROUP BY dt
+UNION ALL
+SELECT 'ARR'                                                                    AS metric_name,
+       TO_CHAR((processed_date + (index || ' months')::INTERVAL)::date, 'YYYY') AS dt,
+       SUM(value)                                                               AS sum
+FROM t2
+GROUP BY dt
+ORDER BY dt;
 
 
