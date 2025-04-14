298b52b46800dea2be2cb62d6696df9de87b29fa
Polina Nebova <polina.nebova@gmail.com>
2025-04-14 20:58:39 +0200

    updated transition_paths

--- transition_paths/transition_paths.sql
+++ transition_paths/transition_paths.sql
@@ -15,6 +15,14 @@ FROM (SELECT customer,
             WHERE customer IN (SELECT customer FROM product_sales WHERE license_type = 'Upgrade')) t1
       WHERE license_type = 'Upgrade') t2
 WHERE previous_product IS NOT NULL
+  AND NOT customer IN (SELECT customer
+                       FROM (SELECT customer,
+                                    license_type,
+                                    ROW_NUMBER() OVER (PARTITION BY customer ORDER BY processed_date) AS rn
+                             FROM product_sales
+                             WHERE customer IN (SELECT customer FROM product_sales WHERE license_type = 'Upgrade')) t1
+                       WHERE rn = 1
+                         AND license_type = 'Upgrade')
 GROUP BY previous_product, upgrade_product
 ORDER BY number_of_transitions DESC
 LIMIT 10;
